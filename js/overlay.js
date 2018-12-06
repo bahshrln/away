bdb432be5d6af346edc0e77a911697834abe378a
Britta Herhaus <britta.herhaus@evlka.de>
2018-12-06 16:56:50 +0100

    change to picture and soundmanager
    instead of video or gif.

--- js/overlay.js
+++ js/overlay.js
@@ -1,54 +1,84 @@
+var sound1 = 'mySound';
+
+var timeout = "";
+
 var timeout2 = "";
 var timeout3 = "";
+var timeout4 = "";
+
 var level = 1;
+
+var counter = 0;
 
 //options for bells
 window.addEventListener(
   "load", function(){
-
     //option1
     document.getElementById("full-screen-menu-toggle").addEventListener(
       "click", toggle);
-
+    //stop sound and overlay by click
     document.getElementById("overlay").addEventListener(
-      "click", stop);
+      "click", stop)
 });
 
 var fade = function() {
   if (level>=0) {
     level-=0.0035;
     timeout3 = setTimeout(fade,100);
-    //timeout5 = setTimeout(stop,21000);
+    timeout5 = setTimeout(stop,30000);
     document.getElementById("overlayimg").style.opacity = level;
   }
 }
 
+var fade2 = function() {
+  if(level<0.3){
+    level=0.8;
+  }
+  document.getElementById("overlayimg").style.opacity = level;
+}
+
 var stop = function() {
-  document.getElementById("overlayimg").style.opacity = 0;
+  document.getElementById("overlayimg").style.display = "none";
   clearTimeout(timeout2);
   clearTimeout(timeout3);
-  document.getElementById("overlay").innerHTML ="";
+  soundManager.stop(sound1);
   document.getElementById("bell-icon").setAttribute("src", "img/gloeckchen_icon.png");
 }
 
 var start = function(){
   //start overlay-video
-  document.getElementById("overlay").innerHTML ="<video id=\"overlayimg\" src=\"media/ZfF_Glockengeläut_Marktkirche.mp4\" autoplay>Ihr Browser kann dieses Video nicht wiedergeben.<br/>Sie können ihn unter <a href=\"#\">Link-Addresse</a> abrufen.</video>";
   document.getElementById("overlayimg").style.display ="block";
   document.getElementById("bell-icon").setAttribute("src", "img/x_icon.png");
-  if(document.getElementById("bell-icon").getAttribute("src")=="img/x_icon.png"){
-    document.getElementById("bell-icon").removeEventListener(
-    "click", start);
-    document.getElementById("bell-icon").addEventListener(
-    "click", stop);
-  }
-  timeout4 = setTimeout(fade, 20000);
-  timeout2 = setTimeout(
-      function(){
-        document.getElementById("overlay").innerHTML ="";
-        document.getElementById("bell-icon").setAttribute("src", "img/gloeckchen_icon.png");
-        clearTimeout(timeout3); clearTimeout(timeout2);
-    }, 35000);
+
+  document.getElementById("overlayimg").setAttribute("src", "img/ZfF_Glockengeläut_Marktkirche.jpg");
+  fade();
+  soundManager.url = 'soundmanager2.js';
+
+  soundManager.onready(
+    function() {
+      soundManager.createSound({
+          id: sound1,
+          url: './media/hannover_marktkirche.mp3'
+      });
+
+      //play and timeout after 30 sec
+      sound = sound1;
+      soundManager.play(sound1);
+      timeout2 = setTimeout(
+        function(){
+          //document.getElementById("overlayimg").setAttribute("src", "img/ZfF_Glockengeläut_Marktkirche_Schluss.jpg");
+          soundManager.stop(sound1); clearTimeout(timeout3); clearTimeout(timeout2);
+
+      }, 30000);
+
+      timeout4 = setTimeout(
+        function(){
+          document.getElementById("overlayimg").setAttribute("src", "img/ZfF_Glockengeläut_Marktkirche_Schluss.jpg");
+          fade2();
+          clearTimeout(timeout4);
+      }, 25000);
+
+    });
 }
 
 var toggle = function (){
@@ -59,3 +89,4 @@ var toggle = function (){
       stop();
     }
 }
+
